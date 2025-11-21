ed34925b07071689662281c5c761911d03d1b980
songhee <songewha23@ewha.ac.kr>
2025-11-22 02:44:58 +0900

    11주차 스프링부프 실습

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,62 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+@Controller
+@Slf4j
+public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        log.info(form.toString());
+        //System.out.println(form.toString());
+        //DTO를 엔티티로
+        Article article=form.toEntity();
+        log.info(article.toString());
+        //System.out.println(article.toString());
+        //리파지터리로 엔티티를 db에 저장
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
+        return "";
+    }
+    @GetMapping("articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id="+id);
+        //id조회해 데이터 가져오기
+        Article articleEntity=articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록
+        model.addAttribute("article",articleEntity);
+        //뷰 페이지 반환
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        //모든데이터 가져오기
+        List<Article> articleEntityList=articleRepository.findAll();
+        //모델에 데이터 등록하기
+        model.addAttribute("articleList",articleEntityList);
+        //뷰 페이지 설정하기
+        return "articles/index";
+    }
+
+}
